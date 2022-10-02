72e4324e8d60ec9c9ce0b5f7e6ee07766d6ced25
Gurpreet <gs109690@gmail.com>
2022-10-01 22:29:56 -0500

    added functions

--- contracts/IpfsNFT.sol
+++ contracts/IpfsNFT.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AccessNFT is ERC721 {
+contract AccessNFT is ERC721, Ownable {
     uint256 public mintPrice;
     uint256 public totalSupply;
     uint256 public maxSupply;
+    uint256 public maxPerWallet;
     bool public isMintEnabled;
     address payable public withdrawWallet;
 
@@ -16,7 +18,31 @@ contract AccessNFT is ERC721 {
         mintPrice = 0.05 ether;
         totalSupply = 0;
         maxSupply = 300;
+        maxPerWallet = 3;
         isMintEnabled = false;
         withdrawWallet = payable(msg.sender);
+    }
+
+    function setMintEnabled(bool _isMintEnabled) external onlyOwner {
+        isMintEnabled = _isMintEnabled;
+    }
+
+    function withdrawBalance() external onlyOwner {
+        (bool success, ) = withdrawWallet.call{value: address(this).balance}("");
+        require(success, "Withdraw failed!");
+    }
+
+    function mint(uint256 _quantity) public payable {
+        require(isMintEnabled, "Mint is not yet available!");
+        require(totalSupply + _quantity <= maxSupply, "Max supply reached!");
+        require(msg.value == _quantity * mintPrice, "Wrong Mint Value");
+        require(walletMints[msg.sender] + _quantity <= maxPerWallet, "Cannot mint more than 2!");
+
+        for (uint256 i = 0; i < _quantity; i++) {
+            uint256 newTokenId = totalSupply + 1;
+            totalSupply++;
+
+            _safeMint(msg.sender, newTokenId);
+        }
     }
 }
